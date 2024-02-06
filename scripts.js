fb4ee4c591ea33ae5b6ad65ecfb6c7b2f982c374
hamza721wolf <143549780+hamza721wolf@users.noreply.github.com>
2024-02-07 01:36:58 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function toggleEffect(effectType) {
+    const button = document.querySelector('.bounce-btn');
+    button.classList.toggle(effectType);
+}
+
+function toggleEffect(effectType){
+    const button = document.querySelector('.fade-btn');
+    button.classList.toggle(effectType);
+}
+
